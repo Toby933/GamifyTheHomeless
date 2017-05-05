a3db60617c3ff718bc04265ccf48a47d8f5caea2
AIE\Toby <Tobykelty@gmail.com>
2017-05-05 16:09:15 +0930

    Initial Commit

---
+++ Assets/Scripts/CycleImage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CycleImage : MonoBehaviour {
+    public Sprite[] sceneImages;
+    private GameObject baseImage;
+    private float timer = 0;
+    private float timerTarget;
+    public float timerSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+        baseImage = GameObject.Find("BaseImage");
+        baseImage.GetComponent<Image>().sprite = sceneImages[Random.Range(0, sceneImages.Length)];
+        timerTarget += timerSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer > timerTarget)
+        {
+            baseImage.GetComponent<Image>().sprite = sceneImages[Random.Range(0, sceneImages.Length)];
+            timerTarget += timerSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/MoveToFront.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MoveToFront : MonoBehaviour {
+    public GameObject[] sceneImages;
+    public string levelName;
+    private GameObject playScene;
+    private GameObject gibMunny;
+
+    void Start()
+    {
+        playScene = GameObject.Find("PlayScene");
+        gibMunny = GameObject.Find("GibMunny");
+        if (playScene != null)
+        {
+            playScene.SetActive(false);
+        }
+        if (gibMunny != null)
+        {
+            gibMunny.SetActive(false);
+        }
+    }
+
+    public void ChangeTarget(string newTarget)
+    {
+        playScene.SetActive(true);
+        levelName = newTarget;
+    }
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+    public void ChangeImage(int number)
+    {
+        for (int i=0; i < sceneImages.Length; i++)
+        {
+            if (i == number)
+            {
+                sceneImages[i].SetActive(true);
+            }
+            else
+            {
+                sceneImages[i].SetActive(false);
+            }
+
+            if (number == 4 || number == 5 && gibMunny != null)
+            {
+                gibMunny.SetActive(true);
+            }
+            else if (gibMunny!=null)
+            {
+                gibMunny.SetActive(false);
+            }
+        }
+    }
+public void Front(GameObject button)
+    {
+        button.transform.SetAsLastSibling();
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("HomelessUICheck");
+    }
+}
